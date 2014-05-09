7a50865c503b1dbecf2b984789abf8eba3482a3a
Pablo Romero <funnyboxification@github.com>
2014-05-09 01:25:45 -0300

    subo project

--- server-con-select-y-hilos/server.c
+++ server-con-select-y-hilos/server.c
@@ -17,7 +17,19 @@
 //#include <arpa/inet.h>
 #include <netdb.h>
 
-#define PUERTO "8547"
+#define PUERTO "8547" // Puertos que se van a escuchar: 8547 , 8548, 8549, 8550
+#define MAX_PUERTOS_ESCUCHADOS 4
+#define MAX_MESSAGE_SIZE 256
+
+//Desarrollo pendiente, proximamente se recibira script de ansisop y se devolvera metadata
+typedef struct {
+	char mensaje[MAX_MESSAGE_SIZE];
+	uint32_t mensajeSize;
+}t_Paquete;
+
+void *thread_handle_client(void*);
+
+int recibirYDeserializar(t_Paquete*, int);
 
 
 int main(int argc, char** argv) {
@@ -29,43 +41,86 @@ int main(int argc, char** argv) {
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_socktype = SOCK_STREAM;
 
+	int server_sockets[MAX_PUERTOS_ESCUCHADOS];
+	int maxDescriptor = -1;
+	for(int i = 0; i < MAX_PUERTOS_ESCUCHADOS; i++) {
+		if( getaddrinfo(NULL, PUERTO+i,&hints, &serverInfo) != 0) {
+				puts("Error en getaddrinfo");
+		}
 
-	if( getaddrinfo(NULL, PUERTO,&hints, &serverInfo) != 0) {
-		puts("Error en getaddrinfo");
-	}
+		if ((server_sockets[i] = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol)) < 0) {
+			puts("Error seteando socket de server");
+			return -1;
+		}
+		if( bind(server_sockets[i], serverInfo->ai_addr, serverInfo->ai_addrlen) < 0) {
+			puts("Error bindeando socket");
+			return -1;
+		}
+		freeaddrinfo(serverInfo);
 
-	int server_socket;
-	if ((server_socket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol)) < 0) {
-		puts("Error seteando socket de server");
-		return -1;
-	}
-
-	if( bind(server_socket, serverInfo->ai_addr, serverInfo->ai_addrlen) < 0) {
-		puts("Error bindeando socket");
-		return -1;
-	}
-	freeaddrinfo(serverInfo);
-
-	if( listen(server_socket, 5) < 0) {
-		puts("Error en el listen");
-		return -1;
+		if( listen(server_sockets[i], 5) < 0) {
+			puts("Error en el listen");
+			return -1;
+		}
+		if(server_sockets[i] > maxDescriptor)
+			maxDescriptor = server_sockets[i];
 	}
 
 	char salir = 0;
 	fd_set readSockSet;
-	fd_set writeSockSet;
-
-	FD_ZERO(readSockSet);
-	FD_ZERO(writeSockSet);
 
 
 	while (!salir) {
+		//Para poder estar al tanto de conexiones entrantes
+		FD_ZERO(readSockSet);
+		FD_SET(STDIN_FILENO,&readSockSet); //Si escribis algo en la consola del server es xq queres salir :B
+		for(int i=0; i<MAX_PUERTOS_ESCUCHADOS ; i++) {
+			FD_SET(server_sockets[i],&readSockSet);
+		}
 
+		int ret;
+		if( (ret = select(maxDescriptor + 1, &readSockSet, NULL, NULL, NULL)) > 0) {
+			if(FD_ISSET(0,&readSockSet)) {
+				puts("Bye bye");
+				getchar();
+				salir = 1;
+			}
+
+			for(int i = 0; i < MAX_PUERTOS_ESCUCHADOS;i++ ) {
+				if(FD_ISSET(server_sockets[i], readSockSet) ) {
+					printf("conexion entrante en puerto %d", PUERTO+i);
+					struct sockaddr_in clienteAddr;
+					socklen_t clienteAddrLen = sizeof(clienteAddr);
+					pthread_t threadid;
+					int clienteSock = accept(server_sockets[i], (struct sockaddr*) &clienteAddr, &clienteAddrLen);
+					if(clienteSock > 0) {
+						pthread_create_thread(&threadid,NULL,thread_handle_client,(void *) &clienteSock);
+					}
+				}
+			}
+		}
 	}
+	for(int i = 0; i < MAX_PUERTOS_ESCUCHADOS; i++)
+		close(server_sockets[i]);
+
+	return 0;
+}
 
 
+void *thread_handle_client(void* param_cS) {
 
+	int clientSocket = *(int *) param_cS;
+	puts("Nuevo cliente conectado.");
+	int bufferSize;
+	char* buffer = malloc((bufferSize = sizeof(uint32_t)));
 
+	//
 
+	return NULL;
+}
 
+int recibirYDeserializar(t_Paquete* paquete, int clientSocket) {
+	int status;
+
+	return status;
 }
